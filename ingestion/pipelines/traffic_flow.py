456566067b58f95ebf1f57b6e0f8762af7264381
xavierkooijman <xavierkooijman@gmail.com>
2026-06-16 12:05:56 +0100

    removed on conlfict do nothing, pipeline idempotency will be enforced at future layers not at raw layer

--- ingestion/pipelines/traffic_flow.py
+++ ingestion/pipelines/traffic_flow.py
@@ -16,8 +16,6 @@ class TrafficFlowPipeline(BaseETLPipeline):
             "source": self.config["source"]["name"],
             "frc": flow_segment_data.get("frc"),
             "tstamp": datetime.now(timezone.utc),
-            "latitude": self.config["source"]["parameters"].get("point", "").split(",")[0],
-            "longitude": self.config["source"]["parameters"].get("point", "").split(",")[1],
             "current_speed_kmh": flow_segment_data.get("currentSpeed"),
             "free_flow_speed_kmh": flow_segment_data.get("freeFlowSpeed"),
             "current_travel_time_s": flow_segment_data.get("currentTravelTime"),
